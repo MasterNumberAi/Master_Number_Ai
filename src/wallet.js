7946a4175f73253c3466c692312c83ad628c95fb
MasterNumberAi <114791781+MasterNumberAi@users.noreply.github.com>
2024-09-24 22:56:41 -0700

    Create wallet.js

---
+++ src/wallet.js
@@ -0,0 +1,28 @@
+import React, { useMemo } from 'react';
+import {
+    ConnectionProvider,
+    WalletProvider,
+    WalletModalProvider,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
+
+const WalletConnection = ({ children }) => {
+    const network = 'mainnet-beta';
+    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+    const wallets = useMemo(() => [new PhantomWalletAdapter()], [network]);
+
+    return (
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets} autoConnect>
+                <WalletModalProvider>
+                    <WalletMultiButton />
+                    {children}
+                </WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    );
+};
+
+export default WalletConnection;
