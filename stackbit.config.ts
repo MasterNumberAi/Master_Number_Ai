14d8f94e7fdb888228a23aa8422ecd2094c1d8d9
MasterNumberAi <114791781+MasterNumberAi@users.noreply.github.com>
2024-09-25 01:28:13 -0700

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,9 +1,10 @@
 import { defineStackbitConfig } from '@stackbit/types';
 
 export default defineStackbitConfig({
-    "stackbitVersion": "~0.6.0",
-    "nodeVersion": "18",
-    "ssgName": "custom",
-    "contentSources": [],
-    "postInstallCommand": "npm i --no-save @stackbit/types"
-})
+    stackbitVersion: "~0.6.0",
+    nodeVersion: "18",
+    ssgName: "custom",
+    contentSources: [],
+    postInstallCommand: "npm install",
+    devCommand: "npm run start",  // Add this line to define the dev server command
+});
