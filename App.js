41ab559ae9dc8bb476cec4573db873f50afaff82
MasterNumberAi <114791781+MasterNumberAi@users.noreply.github.com>
2024-09-24 21:39:26 -0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import WalletConnection from './wallet';
+
+function App() {
+    return (
+        <WalletConnection>
+            <div>
+                <h1>Welcome to Master Number AI</h1>
+                {/* Add more components or content here */}
+            </div>
+        </WalletConnection>
+    );
+}
+
+export default App;
