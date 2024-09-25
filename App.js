68b2e58e10e1c21db2678c8c3e81f05aaeb531ab
MasterNumberAi <114791781+MasterNumberAi@users.noreply.github.com>
2024-09-24 22:37:32 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import WalletConnection from './wallet';
-import ProjectDetails from './ProjectDetails';
 
 function App() {
     return (
         <WalletConnection>
-            <div className="app-container">
+            <div>
                 <h1>Welcome to Master Number AI</h1>
                 <p>This is the beginning of something amazing. Stay tuned for updates on Fomo Frog Coin, Master Number Coin, and much more!</p>
                 <h2>Explore our Projects:</h2>
@@ -13,7 +12,7 @@ function App() {
                     <li>Fomo Frog Coin</li>
                     <li>Master Number Coin</li>
                 </ul>
-                <ProjectDetails />
+                <h3>Connect Phantom Wallet</h3>
             </div>
         </WalletConnection>
     );
