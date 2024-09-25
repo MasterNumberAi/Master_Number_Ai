a86f71e0ff8f2cce12928287a4522b503d3e8ca6
MasterNumberAi <114791781+MasterNumberAi@users.noreply.github.com>
2024-09-24 21:49:53 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import WalletConnection from './wallet';
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 
 function App() {
     return (
         <WalletConnection>
-            <div>
+            <div style={{ textAlign: 'center', padding: '20px' }}>
                 <h1>Welcome to Master Number AI</h1>
-                {/* Add more components or content here */}
+                <p>This is the beginning of something amazing. Stay tuned for updates on Fomo Frog Coin, Master Number Coin, and much more!</p>
+                <h2>Explore our Projects:</h2>
+                <ul>
+                    <li><a href="#">Fomo Frog Coin</a></li>
+                    <li><a href="#">Master Number Coin</a></li>
+                </ul>
+                <WalletMultiButton />
+                <p>Connected wallet: <span id="wallet-address">Not connected</span></p>
             </div>
         </WalletConnection>
     );
