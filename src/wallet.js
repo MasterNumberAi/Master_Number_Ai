bbcef3ce32809c097f244d28de36ac91d20afd91
MasterNumberAi <114791781+MasterNumberAi@users.noreply.github.com>
2024-09-24 23:03:11 -0700

    Update wallet.js

--- src/wallet.js
+++ src/wallet.js
@@ -1,12 +1,18 @@
-import React, { useMemo } from 'react';
+import { useMemo } from 'react';
 import {
     ConnectionProvider,
-    WalletProvider,
+    WalletProvider
+} from '@solana/wallet-adapter-react';
+import {
     WalletModalProvider,
     WalletMultiButton
-} from '@solana/wallet-adapter-react';
-import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+} from '@solana/wallet-adapter-react-ui';
+import {
+    PhantomWalletAdapter
+} from '@solana/wallet-adapter-wallets';
 import { clusterApiUrl } from '@solana/web3.js';
+
+import '@solana/wallet-adapter-react-ui/styles.css';
 
 const WalletConnection = ({ children }) => {
     const network = 'mainnet-beta';
