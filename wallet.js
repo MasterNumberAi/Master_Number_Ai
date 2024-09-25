1608c0a2bd25a3286fcefac1b0b053a7ee2de542
MasterNumberAi <114791781+MasterNumberAi@users.noreply.github.com>
2024-09-24 21:32:57 -0700

    Add wallet.js for handling wallet connections

---
+++ wallet.js
@@ -0,0 +1,34 @@
+import { useMemo } from 'react';
+import {
+    ConnectionProvider,
+    WalletProvider
+} from '@solana/wallet-adapter-react';
+import {
+    WalletModalProvider,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import {
+    PhantomWalletAdapter
+} from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
+
+import '@solana/wallet-adapter-react-ui/styles.css';
+
+const WalletConnection = ({ children }) => {
+    const network = 'mainnet-beta';
+    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+    const wallets = useMemo(() => [new PhantomWalletAdapter()], [network]);
+
+    return (
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets} autoConnect>
+                <WalletModalProvider>
+                    <WalletMultiButton />
+                    {children}
+                </WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    );
+};
+
+export default WalletConnection;
