accff8c876ab15434c8c2ce9b16012c425e39ccd
MasterNumberAi <114791781+MasterNumberAi@users.noreply.github.com>
2024-09-24 22:55:31 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles.css';
 import { WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import WalletConnection from './wallet';
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { ConnectionProvider } from '@solana/wallet-adapter-react';
 import { clusterApiUrl } from '@solana/web3.js';
@@ -17,9 +14,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <ConnectionProvider endpoint={endpoint}>
         <WalletProvider wallets={wallets} autoConnect>
-            <WalletModalProvider>
-                <App />
-            </WalletModalProvider>
+            <App />
         </WalletProvider>
     </ConnectionProvider>
 );
